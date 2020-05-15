0557229de34bc2cf329bc3ec94308bdf0b70539f
Daniel Weidinger <daniel.weidinger@hp-weidinger.at>
2020-05-15 12:52:35 -0700

    update

--- startServing.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo docker run -p 8501:8501 \
-  --mount type=bind,source=/home/daniel/Desktop/DocNet/SavedModels/DocNet,target=/models/docnet \
-  -e MODEL_NAME=docnet -t tensorflow/serving
